ab42895eaaac7e827e7413660ff12613f7793166
golddiamonds <rhall@berkeley.edu>
2014-05-13 20:42:57 -0700

    Will continue on past the correct answer, but does highlight it with FOUND. Doesn't take very long at all.

--- problem9.py
+++ problem9.py
@@ -3,6 +3,7 @@
 #Q: find the product abc of the pythagorean triple for which a + b + c = 1000
 
 #implementation:
+#		lets find the triplet first
 
 class PythagoreanTriples:
 	def __init__(self, maxprod=1000):
@@ -16,4 +17,21 @@ class PythagoreanTriples:
 			return True
 		return False
 
-	
+#generate list of lists of potential triplets
+#a < b < c
+#a + b + c = 1000
+
+triplets = []
+for a in range(1,1000):
+	for b in range(2, 1000):
+		for c in range(3, 1000):
+			if (c > b) and (b > a) and ((a*a) + (b*b) == (c*c)):
+				print a,b,c, a+b+c
+				if (a + b + c) == 1000:
+					print 'FOUND!'
+				#triplets.append([a,b,c])
+
+#print triplets
+#pythag = PythagoreanTriples()
+
+#print pythag.checkTriple(3,4,5)
