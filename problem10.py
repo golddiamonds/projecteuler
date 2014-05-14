5a972245a58eec5f1b92c564ba56acbb8d6e6fd5
golddiamonds <rhall@berkeley.edu>
2014-05-13 20:24:16 -0700

    This one was easy. Probably easier with a prime table. Heh.

--- problem10.py
+++ problem10.py
@@ -1,3 +1,32 @@
 #problem 10
 
 #Q: find the sum of all primes below two million
+
+#implentation:
+#		find primes below 2M
+#		only look at odds
+#		only look at <= ceil(sqrt(n))
+
+from math import ceil, sqrt
+
+primes = [2,3,5]
+
+for n in range(7,2000000): #2000000):
+	#print n
+	#print int(ceil(sqrt(n)) + 1)
+	for j in range(2,int(ceil(sqrt(n))) + 1):
+		#print j
+		if (n % j) == 0:
+			#print j, n, 'not prime!'
+			break
+		if j == int(ceil(sqrt(n))):
+			print 'prime! ', n
+			primes.append(n)
+
+#print primes
+
+prime_sum = 0
+for i in primes:
+	prime_sum += i
+
+print prime_sum
