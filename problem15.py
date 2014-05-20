470e2ecf51c98f3b492953fd9050ef8ef8987710
golddiamonds <rhall@berkeley.edu>
2014-05-20 15:58:32 -0700

    No code for this one. While trying to solve I found it could be done simply. Figured something should be put up anyway.

---
+++ problem15.py
@@ -0,0 +1,7 @@
+# While working on this one, I realized that it could be solved
+# by solving for how many ways 20 "downs" could be arranged with
+# 20 "rights"; basically a combination. Which gives the correct answer.
+# No need for any complicated lists...
+#
+#
+
